15212466fd83484667ecd41d3466712bc834ab9a
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-04 10:38:29 -0300

    :sparkles: feat: Inserindo transação na API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createServer} from "miragejs"
+import { createServer, Model } from "miragejs"
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -8,20 +8,44 @@ const root = ReactDOM.createRoot(
 );
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1, //sempre tem que ter
+          title: "Freelancer de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00")
+        },
+        {
+          id: 2, //sempre tem que ter
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-14 11:00:00")
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = "api"; //Todas URL que tiverem /api/
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all("transaction");
     }) //Rota de busca ou listagem 
+ 
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody) //Schema é o bd
+
+      return schema.create("transaction", data);
+    })
   }
 })
 
