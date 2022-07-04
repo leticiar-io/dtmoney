15cb99942bc2afaf1b22ffd25b916ea6b40d2f68
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-04 11:57:33 -0300

    :sparkles: feat: Adicionando context e finalizando projeto

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,13 @@
 import Modal from "react-modal";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { FormEvent, useContext, useState } from "react";
+
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
-import { Container, TransactionTypeContainer, RadioBox } from "./styles";
-import { FormEvent, useState } from "react";
+
 import { api } from "../../services/api";
+import { useTransactions } from "../../hooks/useTransactions";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -15,74 +18,91 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose
 }: NewTransactionModalProps) {
+  const { createTransaction } = useTransactions();
+
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState(0);
-  const [category, setCategory] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+    await createTransaction({
       title,
-      value,
-      category
-    }
+      amount,
+      category,
+      type
+    });
 
-    api.post("/transactions", data)
+    //Para quando der certo, remover aqueles campos
+    setTitle("");
+    setAmount(0);
+    setCategory("");
+    setType("deposit");
+    
+    onRequestClose();
   }
 
   return (
-    <Modal 
-      isOpen={isOpen} 
+    <Modal
+      isOpen={isOpen}
       onRequestClose={onRequestClose}
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button type="button" onClick={onRequestClose} className="react-modal-close">
-        <img src={closeImg} alt="Fechar Modal"/>
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
       </button>
-        <Container onSubmit={handleCreateNewTransaction}>
-            <h2>Cadastrar transação</h2>
-            <input 
-              placeholder="Título" 
-              value={title} 
-              onChange = {event => setTitle(event.target.value)}
-            />
-            <input 
-              type="number" 
-              placeholder="Valor"
-              value={value} 
-              onChange = {event => setValue(Number(event.target.value))}
-            />
-            <TransactionTypeContainer>
-              <RadioBox 
-                type="button"
-                onClick={() => {setType("deposit");}}
-                isActive={type === "deposit"}
-                activeColor="green"
-              >
-                <img src={incomeImg} alt="Entrada" />
-                <span>Entrada</span>
-              </RadioBox>
-              
-              <RadioBox 
-                type="button" 
-                onClick={() => {setType("withdraw");}}
-                isActive={type === "withdraw"}
-                activeColor="red"
-              >
-                <img src={outcomeImg} alt="Saída" />
-                <span>Saída</span>
-              </RadioBox>
-            </TransactionTypeContainer>
-            <input 
-              placeholder="Categoria"
-              value={category} 
-              onChange = {event => setCategory(event.target.value)}
-              />
-            <button type="submit">Cadastrar</button>
-        </Container>
+      <Container onSubmit={handleCreateNewTransaction}>
+        <h2>Cadastrar transação</h2>
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={amount}
+          onChange={(event) => setAmount(Number(event.target.value))}
+        />
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,18 +1,43 @@
+import React, {useContext} from "react";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
+import { useTransactions } from "../../hooks/useTransactions";
 
 import { Container,  } from "./styles";
 
 export function Summary() {
+  const {transactions} = useTransactions();
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === "deposit") {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+    return acc;
+  },{
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  })
+
   return (
     <Container>
+
       <div>
         <header>
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -20,7 +45,13 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saída" />
         </header>
-        <strong>- R$ 1.259,00</strong>
+        <strong>
+        {summary.withdraws !== 0 ? '-' : ''}
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className="highlight-background">
@@ -28,7 +59,12 @@ export function Summary() {
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$ 16.141,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
